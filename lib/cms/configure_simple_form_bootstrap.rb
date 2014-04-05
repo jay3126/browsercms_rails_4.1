e9aeac07346bcdff5a6f35c4e3d729819e5efb91
Werner Hoier <werner.hoier@elennium.com>
2014-04-05 12:45:28 +0200

    Update SimpleForm initializers to work with new markup

--- lib/cms/configure_simple_form_bootstrap.rb
+++ lib/cms/configure_simple_form_bootstrap.rb
@@ -1,58 +1,131 @@
-# Use this setup block to configure all options available in SimpleForm.
 SimpleForm.setup do |config|
-  config.wrappers :bootstrap, tag: 'div', class: 'control-group row-fluid', error_class: 'error' do |b|
+  config.wrappers :default, tag: :div, class: 'form-group', error_class: 'has-error' do |b|
     b.use :html5
+    b.use :min_max
+    b.use :maxlength
     b.use :placeholder
+
+    b.optional :pattern
+    b.optional :readonly
+
     b.use :label
     b.use :input
-    b.use :error, wrap_with: {tag: 'span', class: 'help-inline'}
-    b.use :hint, wrap_with: {tag: 'p', class: 'help-block'}
+    b.use :error, wrap_with: { tag: :span, class: 'help-block' }
+    b.use :hint, wrap_with: { tag: :p, class: 'help-block' }
   end
 
-  config.wrappers :browsercms, tag: 'div', class: 'control-group row-fluid', error_class: 'error' do |b|
+  config.wrappers :checkbox, tag: :div, class: "form-group", error_class: "has-error" do |b|
     b.use :html5
+    b.optional :readonly
+
+    b.wrapper tag: :div, class: 'checkbox' do |c|
+      c.wrapper tag: :label do |a|
+        a.use :input
+        a.use :label_text
+      end
+    end
+    b.use :error, wrap_with: { tag: :span, class: "help-block" }
+    b.use :hint,  wrap_with: { tag: :p, class: "help-block" }
+  end
+
+  config.wrappers :radio, tag: :div, class: "form-group", error_class: "has-error" do |b|
+    b.use :html5
+    b.optional :readonly
+
+    b.wrapper tag: :div, class: 'radio' do |c|
+      c.wrapper tag: :label do |a|
+        a.use :input
+        a.use :label_text
+      end
+    end
+    b.use :error, wrap_with: { tag: :span, class: "help-block" }
+    b.use :hint,  wrap_with: { tag: :p, class: "help-block" }
+  end
+
+  config.wrappers :horizontal, tag: :div, class: 'form-group', error_class: 'has-error',
+                    label_html: { class: 'col-sm-3 col-md-3 col-lg-2 control-label' } do |b|
+    b.use :html5
+    b.use :min_max
+    b.use :maxlength
     b.use :placeholder
+
+    b.optional :pattern
+    b.optional :readonly
+
+    b.use :label
+    b.wrapper tag: :div, class: 'col-sm-9 col-md-9 col-lg-10' do |i|
+      i.use :input
+      i.use :error, wrap_with: { tag: :span, class: 'help-block' }
+      i.use :hint, wrap_with: { tag: :p, class: 'help-block' }
+    end
+  end
+
+  config.wrappers :horizontal_checkbox, tag: :div, class: "form-group", error_class: "has-error" do |b|
+    b.use :html5
+    b.optional :readonly
+
+    b.wrapper tag: :div, class: 'col-sm-offset-3 col-sm-9 col-md-offset-3 col-md-9 col-lg-offset-2 col-lg-10' do |i|
+      i.wrapper tag: :div, class: 'checkbox' do |c|
+        c.wrapper tag: :label do |a|
+          a.use :input
+          a.use :label_text
+        end
+      end
+      i.use :error, wrap_with: { tag: :span, class: "help-block" }
+      i.use :hint,  wrap_with: { tag: :p, class: "help-block" }
+    end
+  end
+
+  config.wrappers :horizontal_radio, tag: :div, class: "form-group", error_class: "has-error" do |b|
+    b.use :html5
+    b.optional :readonly
+
+    b.wrapper tag: :div, class: 'col-sm-offset-3 col-sm-9 col-md-offset-3 col-md-9 col-lg-offset-2 col-lg-10' do |i|
+      i.wrapper tag: :div, class: 'radio' do |c|
+        c.wrapper tag: :label do |a|
+          a.use :input
+          a.use :label_text
+        end
+      end
+      i.use :error, wrap_with: { tag: :span, class: "help-block" }
+      i.use :hint,  wrap_with: { tag: :p, class: "help-block" }
+    end
+  end
+
+  config.wrappers :inline, tag: :div, class: 'form-group', error_class: 'has-error',
+                    label_html: { class: 'sr-only' } do |b|
+    b.use :html5
+    b.use :min_max
+    b.use :placeholder
+
+    b.optional :pattern
+    b.optional :readonly
+
     b.use :label
     b.use :input
-    b.use :error, wrap_with: {tag: 'span', class: 'help-inline'}
-    b.use :hint, wrap_with: {tag: 'p', class: 'help-block'}
   end
 
-  config.wrappers :checkbox, :tag => 'div', :class => 'control-group row-fluid', :error_class => 'error' do |b|
+  config.wrappers :inline_checkbox, tag: :div, class: "form-group", error_class: "has-error" do |b|
     b.use :html5
-    b.use :placeholder
-    b.use :label_input
-    b.use :hint, :wrap_with => {:tag => 'span', :class => 'help-block'}
-    b.use :error, :wrap_with => {:tag => 'span', :class => 'help-inline'}
-  end
+    b.optional :readonly
 
-  config.wrapper_mappings = { :boolean => :checkbox }
-
-  config.wrappers :prepend, tag: 'div', class: "control-group", error_class: 'error' do |b|
-    b.use :html5
-    b.use :placeholder
-    b.use :label
-    b.wrapper tag: 'div', class: 'input-prepend' do |prepend|
-      prepend.use :input
+    b.wrapper tag: :div, class: 'checkbox' do |c|
+      c.wrapper tag: :label do |a|
+        a.use :input
+        a.use :label_text
+      end
     end
-    b.use :hint, wrap_with: {tag: 'span', class: 'help-block'}
-    b.use :error, wrap_with: {tag: 'span', class: 'help-inline'}
   end
 
-  config.wrappers :append, tag: 'div', class: "control-group", error_class: 'error' do |b|
+  config.wrappers :inline_radio, tag: :div, class: "form-group", error_class: "has-error" do |b|
     b.use :html5
-    b.use :placeholder
-    b.use :label
-    b.wrapper tag: 'div', class: 'input-append' do |append|
-      append.use :input
-    end
-    b.use :hint, wrap_with: {tag: 'span', class: 'help-block'}
-    b.use :error, wrap_with: {tag: 'span', class: 'help-inline'}
-  end
+    b.optional :readonly
 
-  # Wrappers for forms and inputs using the Twitter Bootstrap toolkit.
-  # Check the Bootstrap docs (http://twitter.github.com/bootstrap)
-  # to learn about the different styles for forms and inputs,
-  # buttons and other elements.
-  config.default_wrapper = :bootstrap
+    b.wrapper tag: :div, class: 'radio' do |c|
+      c.wrapper tag: :label do |a|
+        a.use :input
+        a.use :label_text
+      end
+    end
+  end
 end
